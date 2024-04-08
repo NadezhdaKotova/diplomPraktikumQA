2c3a99410208c7594bdc0c5b13379b4f1a09301b
Nadezhda Kotova <kiberlee@yandex.ru>
2024-04-08 14:37:30 +0300

    My second commit

---
+++ track_order_test.py
@@ -0,0 +1,12 @@
+import sending_requests
+
+
+def get_track_number_of_order():
+    track_number = sending_requests.create_new_order()
+    return track_number.json()["track"]
+
+
+def test_create_and_track_order():
+    track_number = get_track_number_of_order()
+    get_response = sending_requests.get_order_by_track(track_number)
+    assert get_response.status_code == 200
